a7c93e7c605e553b5bf230951f4356bac22553b1
leXRishi32 <93484836+leXRishi32@users.noreply.github.com>
2021-12-13 20:43:07 +0530

    Create arrSelectSort Function.cpp

---
+++ arrSelectSort Function.cpp
@@ -0,0 +1,21 @@
+void arrSelectSort(int *arr[], int size)
+{
+    int startScan, minIndex;
+    int *minElem;
+
+    for (startScan = 0; startScan < (size - 1); startScan++)
+    {
+        minIndex = startScan;
+        minElem = arr[startScan];
+        for (int index = startScan + 1; index < size; index++)
+        {
+            if (*(arr[index]) < *minElem)
+            {
+                minElem = arr[index];
+                minIndex = index;
+            }
+        }
+        arr[minIndex] = arr[startScan];
+        arr[startScan] = minElem;
+    }
+}
